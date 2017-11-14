41307c642483f86b494aa13ef0c697434d2c6078
StephC-C <905145707@qq.com>
2017-11-14 21:46:00 +0800

    add some codes to theme and list

--- src/components/backg.vue
+++ src/components/backg.vue
@@ -1,34 +1,52 @@
 <template>
+
   <div class="backg">
+
+  <div class="icon" v-on: click ="changeThemeSwitch">
     <img src="../assets/2.jpg">
   </div>
 
-  <div class="themebn">
-    <img src="./assets/主题.png">
+  <div class="options">
+    <template v-if="ok">
+      <select v-model="theme">
+        <option v-for="item in themes">item</option>
+    </template>
+  </div>
+
+  <div class="backgImg">
+    <img v-bind: src = "theme">
+  </div>
 
   </div>
 </template>
 
 <script>
-  var songsList= [
-    {name: 'I knew you were trouble', singer: 'Taylor Swift'},
-    {name: 'As if it is the last', singer: 'Black Pink'},
-    {name: 'Playing with fire', singer: 'Black Pink'},
-    {name: 'Again', singer: 'YUI'}
-  ]
+  var backg = new Vue({
+    e1: '#backg',
+    data: {
+      themes: ['./assets/2.jpg','./assets/3.jpg','./assets/4.jpg','./assets/5.jpg','./assets/6.jpg','./assets/1.jpg'],
+      theme: './assets/2.jpg',
+      option: false,
+    },
+    method:{
+      changeThemeSwitch: function(){
+        this.option = !this.option;
+      }
+    }
+  })
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .themebn {
-    position: static;
-    left: 600px;
-    top: -600px;
-  }
-  .backg {
+  .backgImg {
     position: static;
     left: 0px;
     top: 0px;
     z-index: -1;
+  }
+  .icon {
+    position: static;
+    right: 0px;
+    bottom: 0px;
   }
 </style>
--- src/components/list.vue
+++ src/components/list.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="menu">
+  <div class="list">
+  <div class="icon">
     <img src="../assets/菜单.png">
   </div>
+
+  <div class="songs">
+  <li v-for="item in songsList">
+  {{item.name}} Singer:{{item.singer}}
+  </li>
 </template>
 
 <script>
-  var songsList= [
+  var list = new Vue({
+    data:{
+    songsList: [
     {name: 'I knew you were trouble', singer: 'Taylor Swift'},
     {name: 'As if it is the last', singer: 'Black Pink'},
     {name: 'Playing with fire', singer: 'Black Pink'},
     {name: 'Again', singer: 'YUI'}
-  ]
+    ],
+    
+    }
+  })
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .menu {
+  .icon {
     position: static;
     left: 0px;
     top: 0px;
