09b5b492e58673eab2994b78ffa0d5733d62e0c4
StephC-C <905145707@qq.com>
2017-11-14 10:38:14 +0800

    nothing happened

---
+++ src/components/backg.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="backg">
+    <img src="../assets/2.jpg">
+  </div>
+
+  <div class="themebn">
+    <img src="./assets/主题.png">
+
+  </div>
+</template>
+
+<script>
+  var songsList= [
+    {name: 'I knew you were trouble', singer: 'Taylor Swift'},
+    {name: 'As if it is the last', singer: 'Black Pink'},
+    {name: 'Playing with fire', singer: 'Black Pink'},
+    {name: 'Again', singer: 'YUI'}
+  ]
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .themebn {
+    position: static;
+    left: 600px;
+    top: -600px;
+  }
+  .backg {
+    position: static;
+    left: 0px;
+    top: 0px;
+    z-index: -1;
+  }
+</style>
---
+++ src/components/menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="menu">
+    <img src="../assets/菜单.png">
+  </div>
+</template>
+
+<script>
+  var songsList= [
+    {name: 'I knew you were trouble', singer: 'Taylor Swift'},
+    {name: 'As if it is the last', singer: 'Black Pink'},
+    {name: 'Playing with fire', singer: 'Black Pink'},
+    {name: 'Again', singer: 'YUI'}
+  ]
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .menu {
+    position: static;
+    left: 0px;
+    top: 0px;
+  }
+</style>
